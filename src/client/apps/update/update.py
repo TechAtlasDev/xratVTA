a9e39ad7852c9b1a0e98b73704397278877be3ea
techatlasdev <srgoqui@gmail.com>
2024-07-26 14:53:29 -0500

    Añadiendo un sistema de control de actualizaciones

--- src/client/apps/update/update.py
+++ src/client/apps/update/update.py
@@ -1,11 +1,15 @@
 import git, requests
 from ..utils.loadConfig import load
+import sys, os
 
 def git_pull(repo_path="../.."):
-    print ("Actualizando...")
     repo = git.Repo(repo_path)
     origin = repo.remotes.origin
     origin.pull()
+
+    # Volviendo a ejecutar el script de manera automatica y cerrando el actual
+    argumentos = sys.argv[:]
+    os.execv(sys.executable, [sys.executable] + argumentos)
 
 def forUpdate(owner, project, local_path="../.."):
     local = str(git.Repo(local_path).head.commit.hexsha)
