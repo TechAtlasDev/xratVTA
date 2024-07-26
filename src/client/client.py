a1385486edf01401a2980785f061349c6d19505b
techatlasdev <srgoqui@gmail.com>
2024-07-26 15:41:27 -0500

    Añadiendo un actualizador y procesador de configuraciones

--- src/client/client.py
+++ src/client/client.py
@@ -8,22 +8,27 @@ PORT = 50007
 
 while True:
     time.sleep(5)
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client.connect((HOST, PORT))
-    client.sendall(b'get')
-
-    # Recibir datos del servidor
-    data = client.recv(1024).decode()
-
     try:
-        objeto = json.loads(data)
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client.connect((HOST, PORT))
+        client.sendall(b'get')
 
-    except json.JSONDecodeError as e:
-        print(f"Error al decodificar el JSON: {e}")
+        # Recibir datos del servidor
+        data = client.recv(1024).decode()
+
+        try:
+            objeto = json.loads(data)
+
+        except json.JSONDecodeError as e:
+            print(f"Error al decodificar el JSON: {e}")
+            client.close()
+            exit()
+
+        x = payload(objeto)
+        processor(client, x)
+
         client.close()
-        exit()
-
-    x = payload(objeto)
-    processor(client, x)
-
-    client.close()
+    
+    # Controlando el error ConnectionRefusedError
+    except ConnectionRefusedError:
+        time.sleep(10)
