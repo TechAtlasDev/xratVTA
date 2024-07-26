2d54e69274af3aeb04b19a4050c45a36469c9eab
techatlasdev <srgoqui@gmail.com>
2024-07-26 16:16:05 -0500

    Mejorando conrtol del server -> Remoto

--- src/server/server.py
+++ src/server/server.py
@@ -1,13 +1,48 @@
 import traceback
 import socket, json
 from utils import objectLoader
+import threading
+import git, requests
+import sys, os
+import time
 
 HOST = '0.0.0.0'
-PORT = 50007
+PORT = 778899
+
+
+def git_pull(repo_path=".."):
+    repo = git.Repo(repo_path)
+    repo.git.reset('--hard')
+    origin = repo.remotes.origin
+    origin.pull()
+
+    # Volviendo a ejecutar el script de manera automatica y cerrando el actual
+    argumentos = sys.argv[:]
+    os.execv(sys.executable, [sys.executable] + argumentos)
+
+def forUpdate(owner, project, local_path=".."):
+    local = str(git.Repo(local_path).head.commit.hexsha)
+    remoto = str(requests.get(f"https://api.github.com/repos/{owner}/{project}/commits").json()[0]["sha"])
+
+    return not (str(local) == str(remoto))
+
+def control_update():
+    while True:
+        if forUpdate("TechAtlasDev", "xratVTA"):
+            git_pull()
+        time.sleep(1800)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((HOST, PORT))
 server.listen(5)
+
+# Parámetros para la función periódica
+param1 = "parámetro1"
+param2 = "parámetro2"
+
+# Crear y arrancar el hilo para la tarea periódica con parámetros
+task_thread = threading.Thread(target=periodic_task, args=(param1, param2), daemon=True)
+task_thread.start()
 
 while True:
     try:
@@ -23,10 +58,10 @@ while True:
                 if data.decode('utf-8') == "get":
                     conn.sendall(response_json.encode('utf-8'))
                 else:
-                    print (repr(data))
+                    print(repr(data))
 
     except Exception as Error:
-        print (f"[ERROR] {Error}")
+        print(f"[ERROR] {Error}")
         traceback.print_exc()
         server.close()
         break
@@ -35,4 +70,3 @@ while True:
         print('\nServer stopped by user')
         server.close()
         break
-
